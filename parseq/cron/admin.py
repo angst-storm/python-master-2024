2190d1b2755e5ad48e187dccca42637393170652
Kiprin Sergei <sergei@jinteki.dev>
2024-10-09 19:02:43 +0500

    added first view and model

--- parseq/cron/admin.py
+++ parseq/cron/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Parser
 
-# Register your models here.
+admin.site.register(Parser)
--- parseq/cron/models.py
+++ parseq/cron/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Parser(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    script = models.FileField()
+    scheduled = models.DateTimeField(null=True)
+    repeat_after = models.DurationField(null=True)
