4b82144efb59d7843f221498ebcbd0cf6da4354d
Kiprin Sergei <sergei@jinteki.dev>
2024-10-10 10:50:11 +0500

    added apscheduler and first django command

---
+++ parseq/cron/management/commands/runapscheduler.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Test"
+
+    def handle(self, *args, **options):
+        print("APScheduler runned")
--- parseq/parseq/settings.py
+++ parseq/parseq/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_dramatiq',
+    'django_apscheduler',
     'cron.apps.CronConfig'
 ]
 
