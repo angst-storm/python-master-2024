a2fbe7af919acedb92c3c0fbb25bb1b9b2c38b0f
Kiprin Sergei <sergei@jinteki.dev>
2024-10-10 14:13:50 +0500

    fixed job scheduling

--- parseq/cron/models.py
+++ parseq/cron/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from .tasks import send_run_actor
 from django.db.models.signals import post_save
-from apscheduler.jobstores.base import JobLookupError
+from apscheduler.jobstores.base import JobLookupError, ConflictingIdError
+from apscheduler.triggers.date import DateTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 
 class Parser(models.Model):
     name = models.CharField(max_length=255)
@@ -10,22 +12,32 @@ class Parser(models.Model):
     scheduled = models.DateTimeField(null=True, blank=True)
     repeat_after = models.DurationField(null=True, blank=True)
 
+    @property
+    def job_id(self):
+        return str(self.id)
+
     @staticmethod
     def post_save(sender, instance, created, **kwargs):
         from .scheduler import scheduler
 
-        job_id = str(instance.id)
-        args = (instance.name,instance.script.path)
+        job_id = instance.job_id
 
         try:
-            scheduler.remove_job(job_id)
+            scheduler.pause_job(job_id)
         except JobLookupError:
             pass
 
         if instance.scheduled is not None:
+            try:
+                scheduler.add_job(id=job_id, func=send_run_actor, args = (instance.name,instance.script.path))
+            except ConflictingIdError:
+                pass
+
             if instance.repeat_after is None:
-                scheduler.add_job(send_run_actor, 'date', args = args, run_date=instance.scheduled, id=job_id)
+                scheduler.reschedule_job(job_id, trigger=DateTrigger(instance.scheduled))
             else:
-                scheduler.add_job(send_run_actor, 'interval', args = args, seconds=instance.repeat_after.seconds, start_date=instance.scheduled, id=job_id)
+                scheduler.reschedule_job(job_id, trigger=IntervalTrigger(seconds=instance.repeat_after.seconds, start_date=instance.scheduled))
+
+            scheduler.resume_job(job_id)
 
 post_save.connect(Parser.post_save, sender=Parser)
