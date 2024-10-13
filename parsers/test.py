45c9997bd6e18af1000819ed3129bc4554648713
Kiprin Sergei <sergei@jinteki.dev>
2024-10-13 16:36:51 +0500

    refactored evewars tests

--- parsers/test.py
+++ parsers/test.py
@@ -9,32 +9,22 @@ class TestCase(unittest.TestCase):
     @responses.activate
     @freeze_time("2024-10-13 00:00:00")
     def test_eve_wars(self):
-        responses._add_from_file(file_path="test-evewars-responses.yaml")
-        expected = {
-            "result.txt": str.encode(
-                """War ID 3: Alliance 1 (AID1) declared war to Corporation 2 (CID2) 5 hours ago
-War ID 2: Corporation 2 (CID2) declared war to Alliance 1 (AID1) 11 hours ago
-"""
-            )
-        }
+        responses._add_from_file(file_path="testdata/evewars/responses.yaml")
 
         output = parse()
 
-        self.assertEqual(expected, output)
+        with open("testdata/evewars/expected.txt", "rb") as expected:
+            self.assertEqual({"result.txt": expected.read()}, output)
 
     @responses.activate
     @freeze_time("2024-10-13 00:00:00")
     def test_eve_wars_real_data(self):
-        responses._add_from_file(file_path="test-evewars-responses-realdata.yaml")
-        expected = {
-            "result.txt": str.encode(
-                "War ID 745134: BLACKFLAG. (S4LTY) declared war to Les deux mousquetaires (LDM.) 4 hours ago\nWar ID 745130: BLACKFLAG. (S4LTY) declared war to 2 Unicorns et a Obelisk (2UEO) 9 hours ago\nWar ID 745129: C A P S T E X T . (CAP) declared war to Friendly LoneWolf Corporation (FLW C) 10 hours ago\nWar ID 745128: Bully Brigade (BULLY) declared war to Friendly LoneWolf Corporation (FLW C) 10 hours ago\nWar ID 745127: Bully Brigade (BULLY) declared war to Codex Signal Prism (CSPHG) 13 hours ago\nWar ID 745126: Smart Deploy (5MART) declared war to Rim Worlds Republic (RIM W) 13 hours ago\nWar ID 745125: C A P S T E X T . (CAP) declared war to Codex Signal Prism (CSPHG) 14 hours ago\nWar ID 745124: C A P S T E X T . (CAP) declared war to Job's Done. (.JD.) 21 hours ago\nWar ID 745123: Bully Brigade (BULLY) declared war to Job's Done. (.JD.) 21 hours ago\nWar ID 745122: BLACKFLAG. (S4LTY) declared war to Blood Drive (BLUD) 22 hours ago\n"
-            )
-        }
+        responses._add_from_file(file_path="testdata/evewars/responses-realdata.yaml")
 
         output = parse()
 
-        self.assertEqual(expected, output)
+        with open("testdata/evewars/expected-realdata.txt", "rb") as expected:
+            self.assertEqual({"result.txt": expected.read()}, output)
 
 
 if __name__ == "__main__":
